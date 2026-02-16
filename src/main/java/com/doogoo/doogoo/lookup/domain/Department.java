f1468bb462dc90ce11a08922b326b65bfc77221d
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-02-17 00:58:21 +0900

    chore: 디랙토리 구조 변경

---
+++ src/main/java/com/doogoo/doogoo/lookup/domain/Department.java
@@ -0,0 +1,17 @@
+package com.doogoo.doogoo.lookup.domain;
+
+public enum Department {
+    DEP_1("dep_1", "컴퓨터공학과"),
+    DEP_2("dep_2", "소프트웨어학과");
+
+    private final String id;
+    private final String displayName;
+
+    Department(String id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public String id() { return id; }
+    public String displayName() { return displayName; }
+}
