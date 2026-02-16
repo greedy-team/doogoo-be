8e822fb17515d8b8f31cdb5c632a7856bbda2bcc
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-02-17 00:58:48 +0900

    chore: 디랙토리 변경

--- src/main/java/com/doogoo/doogoo/academic/infrastructure/AcademicNoticeRepository.java
+++ src/main/java/com/doogoo/doogoo/academic/infrastructure/AcademicNoticeRepository.java
@@ -1,5 +1,6 @@
-package com.doogoo.doogoo.academic.domain;
+package com.doogoo.doogoo.academic.infrastructure;
 
+import com.doogoo.doogoo.academic.domain.AcademicNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AcademicNoticeRepository extends JpaRepository<AcademicNotice, String> {
