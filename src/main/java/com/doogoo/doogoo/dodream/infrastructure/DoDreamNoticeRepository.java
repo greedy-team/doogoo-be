bc51068609e68047f7edfb36c52fc0e0a4170a0a
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-02-17 00:58:48 +0900

    chore: 디랙토리 변경

--- src/main/java/com/doogoo/doogoo/dodream/infrastructure/DoDreamNoticeRepository.java
+++ src/main/java/com/doogoo/doogoo/dodream/infrastructure/DoDreamNoticeRepository.java
@@ -1,5 +1,6 @@
-package com.doogoo.doogoo.dodream.domain;
+package com.doogoo.doogoo.dodream.infrastructure;
 
+import com.doogoo.doogoo.dodream.domain.DoDreamNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoDreamNoticeRepository extends JpaRepository<DoDreamNotice, String> {
