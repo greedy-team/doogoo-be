6263f9558722aecb354ef6b0e2d327a10aead6dd
user <user@MacBook-Pro-2.local>
2026-03-09 18:18:05 +0900

    bug:URL 경로 수정

--- src/main/java/com/doogoo/doogoo/global/config/OpenApiConfig.java
+++ src/main/java/com/doogoo/doogoo/global/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.doogoo.doogoo.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,10 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("두구두구 API")
                         .version("1.0")
-                        .description("학사/두드림 공지 ICS 캘린더 구독 및 조회 API"));
+                        .description("학사/두드림 공지 ICS 캘린더 구독 및 조회 API"))
+                .servers(List.of(
+                        new Server().url("https://www.sejongdoogoo-api.com").description("배포 서버"),
+                        new Server().url("http://localhost:8080").description("로컬 서버")
+                ));
     }
 }
